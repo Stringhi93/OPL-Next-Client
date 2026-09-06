9f83292dc93107272795e19ac04a21c4e91bffef
Arthur <127615157+Stringhi93@users.noreply.github.com>
2026-09-06 15:07:55 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <debug.h>
+#include <kernel.h>
+#include <sifrpc.h>
 
 #include "input.h"
 #include "ui.h"
@@ -10,13 +12,18 @@ int main(int argc, char *argv[])
     (void)argv;
 
     /*
-     * Inicializa a tela de texto do PS2SDK.
+     * Inicializa os serviços RPC necessários ao controle.
+     */
+    SifInitRpc(0);
+
+    /*
+     * Inicializa tela.
      */
     init_scr();
     scr_clear();
 
     /*
-     * Inicializa o controle.
+     * Inicializa controle.
      */
     input_init();
 
@@ -27,17 +34,21 @@ int main(int argc, char *argv[])
 
     /*
      * Loop principal.
-     *
-     * Nao usamos SleepThread nem DelayThread.
-     * A V0.1 ja demonstrou que a tela de texto funciona
-     * dessa maneira no seu PS2.
      */
     while (1)
     {
         input_update();
         ui_update();
         ui_render();
+
+        /*
+         * Pequena pausa para não ocupar 100% da EE.
+         * 1 tick = aproximadamente 1 microsegundo.
+         */
+        DelayThread(10000);
     }
+
+    input_shutdown();
 
     return 0;
 }
