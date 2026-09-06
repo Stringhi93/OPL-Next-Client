e5e5b83698d1e02efd3c4953cdef7371b78938c8
Arthur <127615157+Stringhi93@users.noreply.github.com>
2026-09-06 15:48:22 -0400

    Delete network.h

--- network.h
+++
@@ -1,9 +0,0 @@
-#ifndef NETWORK_H
-#define NETWORK_H
-
-int network_init(void);
-int network_is_ready(void);
-int network_test_server(const char *ip, int port);
-void network_shutdown(void);
-
-#endif
