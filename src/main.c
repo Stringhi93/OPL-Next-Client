f8eb503adfd87b7aca56bc33892925cbd6e9281e
Arthur <127615157+Stringhi93@users.noreply.github.com>
2026-09-05 21:32:13 -0400

    Atualizar o main.c

--- src/main.c
+++ src/main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <debug.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     init_scr();
 
-    scr_printf("================================\n");
-    scr_printf("       OPL NEXT CLIENT\n");
-    scr_printf("================================\n\n");
+    scr_clear();
 
-    scr_printf("PS2 ELF iniciado com sucesso!\n");
+    scr_printf("========================================\n");
+    scr_printf("          OPL NEXT CLIENT\n");
+    scr_printf("========================================\n\n");
+
+    scr_printf("ELF iniciado com sucesso!\n\n");
+
     scr_printf("Projeto: OPL Next\n");
+    scr_printf("Plataforma: PlayStation 2\n");
     scr_printf("Servidor: TV Box\n");
-    scr_printf("Modo: SMB / Rede\n\n");
+    scr_printf("Modo futuro: Rede / SMB\n\n");
 
-    scr_printf("Inicializacao de rede sera adicionada\n");
-    scr_printf("na proxima etapa.\n");
+    scr_printf("----------------------------------------\n");
+    scr_printf("Build test OK!\n");
+    scr_printf("----------------------------------------\n");
 
     while (1)
     {
+        /* Programa permanece executando. */
     }
 
     return 0;
