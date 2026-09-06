7f701e262d2665da4f5d81099324e0abab186269
Arthur <127615157+Stringhi93@users.noreply.github.com>
2026-09-06 16:49:26 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,24 +11,50 @@ int main(int argc, char *argv[])
     (void)argc;
     (void)argv;
 
+    /*
+     * Inicializa RPC.
+     */
     SifInitRpc(0);
 
+    /*
+     * Inicializa tela.
+     */
     init_scr();
     scr_clear();
 
+    /*
+     * Inicializa controle.
+     */
     input_init();
 
-    network_init();
+    /*
+     * Inicializa rede.
+     */
+    if (!network_init())
+    {
+        scr_printf("ERRO: falha ao inicializar rede.\n");
+        scr_printf("Pressione START para continuar.\n");
+    }
 
+    /*
+     * Inicializa interface.
+     */
     ui_init();
 
+    /*
+     * Loop principal.
+     *
+     * ui_update() ja chama input_update().
+     */
     while (1)
     {
-        input_update();
         ui_update();
         ui_render();
     }
 
+    /*
+     * Limpeza.
+     */
     network_shutdown();
     input_shutdown();
 
